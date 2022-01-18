0feb40484ff6b760772e160d6dcb8bd6f821c95b
Ryan Tan <ryantanlien99@gmail.com>
2022-01-18 21:54:46 +0800

    Butler Level 2
    Level 2. Add, List
    Added the ability to store whatever text entered by the user and display them back to the user when requested.

--- src/main/java/Butler.java
+++ src/main/java/Butler.java
@@ -2,6 +2,9 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Butler {
+
+    ArrayList<String> arrayList;
+
     public static void main(String[] args) {
         Butler butler = new Butler();
         String greetings = butler.greet();
@@ -15,19 +18,39 @@ public class Butler {
                 System.out.println("Farewell Master. Glad to be of service.");
                 try {
                     Thread.sleep(1000);
-                } catch (InterruptedException exception){
+                } catch (InterruptedException exception) {
                     exception.printStackTrace();
                 }
                 System.exit(0);
             }
-            System.out.println(input);
+            if (input.equals("list")) {
+                System.out.println(butler.viewList());
+                continue;
+            }
+            System.out.println(butler.addToList(input));
         }
     }
 
     public Butler() {
+        arrayList = new ArrayList<>();
     }
 
     public String greet() {
         return "Greetings! I'm Butler!\n" + "What can I do for you today Master?\n";
+    }
+
+    public String addToList(String string) {
+        arrayList.add(string);
+        return "added: " + string;
+    }
+
+    public String viewList() {
+        int i = 0;
+        String output = "";
+        while (i < arrayList.size()) {
+            output += String.valueOf(i + 1) + ". " + arrayList.get(i) + "\n";
+            i++;
+        }
+        return output;
     }
 }
