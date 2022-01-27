4f913710842eda1b221d4a6bf004ba4c695dbc58
Ryan Tan <ryantanlien99@gmail.com>
2022-01-28 04:15:11 +0800

    Add find command functionality
    Users can now find tasks in their list in the chatbot that contains the provided substring in their description.
    
    New findCommand command object added to adhere to OOP principles.

---
+++ src/main/java/butler/command/FindCommand.java
@@ -0,0 +1,19 @@
+package butler.command;
+
+import butler.TaskList;
+import butler.util.Storage;
+import butler.util.Ui;
+
+public class FindCommand extends Command {
+    private String searchString;
+
+    public FindCommand(String searchString) {
+        this.searchString = searchString;
+    }
+
+    @Override
+    public void execute(TaskList taskList, Storage storage, Ui ui) {
+        String message = taskList.find(searchString);
+        ui.printMessage(message);
+    }
+}
